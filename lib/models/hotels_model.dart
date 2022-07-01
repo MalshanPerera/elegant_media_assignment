d41db88d9a76f1638f337f49ff44794f496190db
MalshanPerera <malshanperera3@gmail.com>
2022-07-02 00:15:28 +0530

    Rename the model to a meaning full name and final commit.

---
+++ lib/models/hotels_model.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+
+import '../helpers/app_strings.dart';
+
+Hotels hotelsFromJson(String str) => Hotels.fromJson(json.decode(str));
+
+class Hotels {
+  final int status;
+  final List<Data> data;
+
+  Hotels({
+    required this.status,
+    required this.data,
+  });
+
+  factory Hotels.empty() => Hotels(
+        status: 0,
+        data: [],
+      );
+
+  factory Hotels.fromJson(Map<String, dynamic> json) => Hotels(
+        status: json["status"] ?? 0,
+        data: List<Data>.from(
+          json["data"] == null ? [] : json["data"].map((x) => Data.fromJson(x)),
+        ),
+      );
+}
+
+class Data {
+  final int id;
+  final String title;
+  final String description;
+  final String address;
+  final String postcode;
+  final String phoneNumber;
+  final String latitude;
+  final String longitude;
+  final Image image;
+
+  Data({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.address,
+    required this.postcode,
+    required this.phoneNumber,
+    required this.latitude,
+    required this.longitude,
+    required this.image,
+  });
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        id: json["id"] ?? 0,
+        title: json["title"] ?? "",
+        description: json["description"] ?? "",
+        address: json["address"] ?? "",
+        postcode: json["postcode"] ?? "",
+        phoneNumber: json["phoneNumber"] ?? "",
+        latitude: json["latitude"] ?? "",
+        longitude: json["longitude"] ?? "",
+        image: Image.fromJson(json["image"] ?? {}),
+      );
+}
+
+class Image {
+  final String small;
+  final String medium;
+  final String large;
+
+  Image({
+    required this.small,
+    required this.medium,
+    required this.large,
+  });
+
+  factory Image.fromJson(Map<String, dynamic> json) => Image(
+        small: json["small"] ?? PLACEHOLDER_IMAGE,
+        medium: json["medium"] ?? PLACEHOLDER_IMAGE,
+        large: json["large"] ?? PLACEHOLDER_IMAGE,
+      );
+}
