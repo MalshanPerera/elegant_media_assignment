d11bc81d2217a0410d464ce51c374f9b1a9383e2
MalshanPerera <malshanperera3@gmail.com>
2022-07-01 23:44:26 +0530

    Add SingleChildScrollView

--- lib/views/details_view.dart
+++ lib/views/details_view.dart
@@ -31,51 +31,53 @@ class DetailsView extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.only(
-          top: Utils.getHeight(25.0),
-          left: Utils.getWidth(20.0),
-          right: Utils.getWidth(20.0),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: Utils.getHeight(20.0),
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: CachedImage(
-                width: Utils.bodyWidth,
-                height: Utils.getHeight(250.0),
-                imageURL: data.image.large,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: Utils.getHeight(25.0),
+            left: Utils.getWidth(20.0),
+            right: Utils.getWidth(20.0),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: Utils.getHeight(20.0),
               ),
-            ),
-            SizedBox(
-              height: Utils.getHeight(20.0),
-            ),
-            Text(
-              data.title,
-              style: const TextStyle(
-                fontSize: 20.0,
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: CachedImage(
+                  width: Utils.bodyWidth,
+                  height: Utils.getHeight(250.0),
+                  imageURL: data.image.large,
+                ),
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: Utils.getHeight(25.0),
-            ),
-            SizedBox(
-              height: Utils.getHeight(10.0),
-            ),
-            Text(
-              data.description,
-              style: TextStyle(
-                fontSize: 15.0,
-                color: Colors.grey.withOpacity(0.8),
+              SizedBox(
+                height: Utils.getHeight(20.0),
               ),
-              textAlign: TextAlign.center,
-            ),
-          ],
+              Text(
+                data.title,
+                style: const TextStyle(
+                  fontSize: 20.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: Utils.getHeight(25.0),
+              ),
+              SizedBox(
+                height: Utils.getHeight(10.0),
+              ),
+              Text(
+                data.description,
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.grey.withOpacity(0.8),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
