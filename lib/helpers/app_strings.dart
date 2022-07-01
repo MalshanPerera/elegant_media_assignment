7b51d1c2c81092801d1453fd83e8b70e1a537396
MalshanPerera <malshanperera3@gmail.com>
2022-07-01 23:38:31 +0530

    Refactor Code

--- lib/helpers/app_strings.dart
+++ lib/helpers/app_strings.dart
@@ -1,4 +1,6 @@
-const APP_NAME = 'Cat Hub';
+const APP_NAME = 'Elegant Media Assignment';
+
+/// Error Strings
 const String INVALID_CRED = "Invalid email or password";
 const String REQUEST_ERROR = 'Failed to send request. Please try again';
 
@@ -10,11 +12,11 @@ const String HTTP_ERROR = 'Oops! An error has occurred';
 const String UNKNOWN_ERROR = 'Unknown Error';
 const String TERMS_AND_CONDITIONS_ERROR = '"You must agree to the terms and conditions"';
 
+/// Placeholder Images
 const String PLACEHOLDER_IMAGE =
     'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
 
 /// Route Strings
-
 const String INITIAL_ROUTE = '/';
 const String LOGIN_ROUTE = '/login';
 const String HOME_ROUTE = '/home';
--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../helpers/app_colors.dart';
 import '../helpers/app_utils.dart';
+import '../helpers/app_strings.dart' show APP_NAME;
 import '../view_models/auth_view_model/auth_view_model.dart';
 
 class SplashView extends StatefulWidget {
@@ -24,10 +26,16 @@ class _SplashViewState extends State<SplashView> {
     //Set the screen sizes and the static util variables
     Utils.setScreenSizes(context);
 
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
       body: Center(
         child: Text(
-          'Cat Hub',
+          APP_NAME,
+          style: TextStyle(
+            fontSize: Utils.getHeight(22.0),
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
       ),
     );
